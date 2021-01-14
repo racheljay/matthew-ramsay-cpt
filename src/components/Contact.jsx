2a926bb0a7b8975bda61763a101b421fa4fac22b
Rachel <racheljay42@gmail.com>
2021-01-14 16:38:45 -0500

    add profile pic, alter carosel sizing, fixed contact styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,17 +6,30 @@ function Contact() {
 
             <h1 className="text-center pt-3">No memberships. No Contracts.</h1>
             <hr />
-            <h2 className="text-center">Interested in personal training? Contact me to schedule your complimentary consultation session.</h2>
+            <div className="row">
+            <div className="col-xl-2"></div>
+            <h3 className="text-center p-2 col-xl-8">Interested in personal training? Contact me to schedule your complimentary consultation session.</h3>
+            <div className="col-xl-2"></div>
+            </div>
             <h3 className="text-center pt-3 pb-3"><a href="mailto:ramsayfitness@gmail.com" target="_blank">ramsayfitness@gmail.com</a></h3>
 
-            <h4 className="pt-3 pb-3">
+            <h3 className="pt-3 pb-3 text-center">
                 I offer 30-minute and/or 1-hour sessions weekly in two forms:
-            </h4>
+            </h3>
             
+            <div className="row">
+            <div className="col-xl-2"></div>
+
+            <div className="col-xl-8">
+
                 <ol>
                     <li>In person training at Fitness Plus. The facility is located at 831 National Avenue, Lexington, KY 40502</li>
                     <li>Virtual training over video conferencing platforms such as Zoom, Google Duo, Google Meets, and Skype</li>
                 </ol>
+            </div>
+            <div className="col-xl-2"></div>
+
+            </div>
             
         </div>
     )
